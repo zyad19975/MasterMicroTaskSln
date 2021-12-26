e612a5d12dd23f7a56828abb432148fc6faf282e
zyad19975 <zyad19975@gmail.com>
2021-12-26 19:49:11 +0200

    init env and testing for Ploting fn

---
+++ Task01_Plotter/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void makePlot();
+    void on_pushButton_clicked();
+
+    void on_textEdit_textChanged();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
